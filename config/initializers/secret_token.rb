0b9c2ae18ff6900d36a3231b49744f647273f021
painkiller778 <painkiller778@gmail.com>
2012-07-31 17:44:38 -0300

    Configuracao completa, layout modificado, controlador basico e roteamento basicas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog4soft::Application.config.secret_token = '98ff1394be74b9573f178a312e09e78ea74ca4813386bb8f5ba67652ccd11f556fc0600f188173909ebb492d9efb12d03e417904741f795b8b0274eaba3287ad'
